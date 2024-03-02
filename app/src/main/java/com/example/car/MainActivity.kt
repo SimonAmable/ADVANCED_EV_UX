2bfbe0594681707a0e7d80c9fc5a08180e6fda99
Dylan Loree <dylan.loree@gmail.com>
2024-03-02 15:25:11 -0500

    Basic Bottom Nav Setup
    -Added basic layout of app with bottom navbar

---
+++ app/src/main/java/com/example/car/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.car
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.car.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        replaceFragment(Map())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.map -> replaceFragment(Map())
+                R.id.settings -> replaceFragment(Settings())
+
+                R.id.accessories -> replaceFragment(Accessories())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+
+
+    private fun replaceFragment(fragment : Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+
+    }
+
+}
